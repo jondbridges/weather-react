9b7eba90f0fa0b262f54b8f6a5808abfcd1c0540
Bridges, Jonathan D <Jonathan.Bridges@Mercy.Net>
2016-09-25 15:18:45 -0500

    move private functions down in weather service

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -4,6 +4,16 @@ import moment from 'moment';
 const FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
 const APP_ID = 'f6ea15ddd1b9a6e5f6911074912b7eb6';
 
+class WeatherService {
+  static getForecast(cityState) {
+    return getFiveDayWeatherData(cityState)
+      .then(resp => {
+        return resp.data.list
+          .map(day => convertToForecastDay(day));
+      });
+  }
+}
+
 function getFiveDayWeatherData(cityState) {
   return axios.get(`${FORECAST_URL}?q=${cityState}&mode=json&appid=${APP_ID}&cnt=5`);
 }
@@ -14,17 +24,6 @@ function convertToForecastDay(day) {
     date: moment.unix(day.dt).format('dddd, MMM D'),
     conditionIcon: day.weather[0].icon
   };
-}
-
-class WeatherService {
-
-  static getForecast(cityState) {
-    return getFiveDayWeatherData(cityState)
-      .then(response => {
-        return response.data.list
-          .map(day => convertToForecastDay(day));
-      });
-  }
 }
 
 export default WeatherService;
