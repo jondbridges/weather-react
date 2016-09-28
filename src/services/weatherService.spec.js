bf755355277b9e8a76f7c93778aafb8592080d46
Bridges, Jonathan D <Jonathan.Bridges@Mercy.Net>
2016-09-28 18:56:23 -0500

    adds fail test for weather service spec

--- src/services/weatherService.spec.js
+++ src/services/weatherService.spec.js
@@ -1,44 +1,51 @@
 import WeatherService, {FORECAST_URL} from './weatherService';
 import weatherServiceTestData from './weatherServiceTestData';
-import mockAxios from 'axios';
-
-// Mock axios get http call to return the test data
-// mockAxios.get = jest.fn(() => {
-//   return new Promise(
-//     function (resolve) {
-//       resolve({data: weatherServiceTestData });
-//     }
-//   );
-// });
+import axios from 'axios';
 
 describe('getForecast', () => {
 
-  it('calls api with city name and a count of 5', () => {
-    let cityName = 'Seattle';
-    spyOn(mockAxios, 'get').and.returnValue(
-      new Promise(
-        resolve => {
-          resolve({data: weatherServiceTestData });
-      })
-    );
+  describe('when successful API call', () => {
 
-    WeatherService.getForecast(cityName);
+    beforeEach(() => {
+      spyOn(axios, 'get').and.returnValue(
+        new Promise(
+          resolve => { resolve({data: weatherServiceTestData }); }
+        )
+      );
+    });
 
-    expect(mockAxios.get)
-      .toHaveBeenCalledWith(FORECAST_URL + '&q=' + cityName + '&cnt=5');
+    it('calls api with city name and a count of 5', () => {
+      let cityName = 'Seattle';
+
+      WeatherService.getForecast(cityName);
+
+      expect(axios.get)
+        .toHaveBeenCalledWith(FORECAST_URL + '&q=' + cityName + '&cnt=5');
+    });
+
+    it('returns a list of 5 forecast days', () => {
+      WeatherService.getForecast('St. Louis')
+        .then(days => {
+          expect(days.length).toBe(5);
+        });
+    });
   });
 
-  it('returns a list of 5 forecast days', () => {
-    spyOn(mockAxios, 'get').and.returnValue(
-      new Promise(
-        resolve => {
-          resolve({data: weatherServiceTestData });
-        })
-    );
-    WeatherService.getForecast('St. Louis')
-      .then(days => {
-        expect(days.length).toBe(5);
-      });
+  describe('when API call errors', () => {
+
+    it('no days are returned', () => {
+      spyOn(axios, 'get').and.returnValue(
+        new Promise(
+          (resolve, reject) => { reject('error occured'); }
+        )
+      );
+
+      WeatherService.getForecast('Boise, ID')
+        .then(days => {
+          expect(days).not.toBeDefined();
+        });
+    });
   });
+
 });
 
