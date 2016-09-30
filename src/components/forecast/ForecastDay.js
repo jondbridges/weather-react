f96c7df5eab11f93c8da4e6db5f4ff1ad03b187c
Bridges, Jonathan D <Jonathan.Bridges@Mercy.Net>
2016-09-29 19:04:11 -0500

    moves img src for weather icon to variable to be easier to read

--- src/components/forecast/ForecastDay.js
+++ src/components/forecast/ForecastDay.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Forecast.css';
 
 const ForecastDay = ({forecastDay}) => {
+  const weatherIconUrl = `${process.env.PUBLIC_URL}/images/weather-icons/${forecastDay.conditionIcon}.svg`;
+  
   return (
     <div className="Forecast-day">
-      <img className="center-block Forecast-weather-icon"
-           src={process.env.PUBLIC_URL + "/images/weather-icons/" + forecastDay.conditionIcon + ".svg"}
-           alt="Weather" />
+      <img className="center-block Forecast-weather-icon" src={weatherIconUrl} alt="Weather" />
       <div className="text-center">{forecastDay.date}</div>
     </div>
   );
